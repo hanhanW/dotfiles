2ce36bc5b88f276863ace22ee6a146a30159796f
hanhanW <hanhan0912@gmail.com>
2026-02-04 12:36:01 -0800

    Update bootstrap script to use install.sh
    Remove python dependency check, simplify to just git.

--- fetch.sh
+++ fetch.sh
@@ -1,21 +1,13 @@
-if ! command -v apt-get >/dev/null 2>&1; then
-  echo $'\e[1;31mNo apt-get?!\e[m'
-  exit 1
-fi
+#!/usr/bin/env bash
+# Bootstrap dotfiles on a new machine
+set -e
 
 if ! command -v git >/dev/null 2>&1; then
-  echo $'\e[1;33mgit not installed, installing...\e[m'
-  sudo apt-get update
-  sudo apt-get -y install git
-fi
-
-if ! command -v python >/dev/null 2>&1; then
-  echo $'\e[1;33mpython not installed, installing...\e[m'
-  sudo apt-get update
-  sudo apt-get -y install python-is-python3
+    echo "git not installed. Please install git first."
+    exit 1
 fi
 
 git clone https://github.com/hanhanW/dotfiles ~/dotfiles
 cd ~/dotfiles
 git remote set-url --push origin git@github.com:hanhanW/dotfiles.git
-./install
+./install.sh
