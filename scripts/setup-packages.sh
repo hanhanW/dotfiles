93cd0f748253d7ec62e7a8c8cf5f854ebc8195ad
hanhanW <hanhan0912@gmail.com>
2026-03-04 11:14:31 -0800

    Add ccache to setup-packages.sh

--- scripts/setup-packages.sh
+++ scripts/setup-packages.sh
@@ -13,8 +13,9 @@ sudo apt-get update
 sudo apt-get install -y build-essential cmake htop
 
 # Only install if not present
-command -v zsh  >/dev/null 2>&1 || sudo apt-get install -y zsh
-command -v tmux >/dev/null 2>&1 || sudo apt-get install -y tmux
-command -v vim  >/dev/null 2>&1 || sudo apt-get install -y vim
+command -v zsh    >/dev/null 2>&1 || sudo apt-get install -y zsh
+command -v tmux   >/dev/null 2>&1 || sudo apt-get install -y tmux
+command -v vim    >/dev/null 2>&1 || sudo apt-get install -y vim
+command -v ccache >/dev/null 2>&1 || sudo apt-get install -y ccache
 
 echo "Packages installed!"
